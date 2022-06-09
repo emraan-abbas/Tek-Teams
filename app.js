062205f958161acc608761fa7998f27984949dd1
emraan-abbas <imranabbas67@gmail.com>
2022-06-09 20:36:07 +0500

    Requiring Socket and Setting up CORS

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const http = require('http');
+const socket = require('socket.io');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const routes = require('./Routes/index');
 const dotenv = require('dotenv');
+const { messenger } = require('./socket.io/socket')
 
 const app = express();
+const server = http.createServer(app); // Express use CreateServer under the hood to create Server
+const io = socket(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,6 +25,7 @@ app.use(bodyParser.json());
 
 // CORS
 app.use(cors());
+
 
 // DOT-ENV
 dotenv.config();
@@ -34,10 +45,26 @@ mongoose.connect('mongodb://127.0.0.1:27017/tekteams', {
 // All Routes
 app.use('/tekteams', routes)
 
-
-
+// Server Creation
 const PORT = 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Tek-Teams is running on Port: ${PORT}`);
 });
+
+
+// Socket IO
+messenger(io);
+
+// io.on('connection', (socket) => {
+//   console.log('User Connected');
+
+//   socket.on('disconnect', () => {
+//     console.log('User Disconnected');
+//   });
+
+//   socket.on('my message', (msg) => {
+//     io.emit('my broadcast', `Server: ${msg}`)
+//   })
+
+// });
 
