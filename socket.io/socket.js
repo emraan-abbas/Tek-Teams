9e12635ad1c3a647a9daec70ab34ac66a4265594
emraan-abbas <imranabbas67@gmail.com>
2022-06-15 21:02:04 +0500

    Broadcast. Emit

--- socket.io/socket.js
+++ socket.io/socket.js
@@ -1,5 +1,5 @@
 const user = require('../Controllers/user.controller')
-const chat = require('../Controllers/message.controller')
+const chat = require('../Controllers/chat.controller')
 
 
 // Socket IO
@@ -11,6 +11,8 @@ exports.messenger = (io) => {
     socket.on('New Message', (data) => {
       console.log(data)
       chat.postMessage(data);
+
+      socket.broadcast.emit('New Message', data)
     })
 
   });
