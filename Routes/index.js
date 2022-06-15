6a4a7fac46d4499b57988fade43f144753a38b80
emraan-abbas <imranabbas67@gmail.com>
2022-06-15 21:03:37 +0500

    Added Chat Route

--- Routes/index.js
+++ Routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express();
 
 const user = require('./user.routes');
+const chat = require('./chat.routes');
 
 
 router.use('/api', user);
+router.use('/api', chat);
 
 module.exports = router
