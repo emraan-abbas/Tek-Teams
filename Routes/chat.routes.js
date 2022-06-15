2a08c9cdc32f050c695bdb7c26c8bbe331f3c9e7
emraan-abbas <imranabbas67@gmail.com>
2022-06-15 21:03:23 +0500

    Get Messages against Sender/ Reciver ID Route

--- Routes/chat.routes.js
+++ Routes/chat.routes.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const router = express();
 
-const chat = require('../Controllers/message.controller')
+const chat = require('../Controllers/chat.controller')
 
-router.get('/chats')
-router.get('/chats/:id') // Get messages by room id
-router.post('/chats/message', chat.postMessage) //Initiate
-router.post('/chats/:id/message') // Send message in specific room
-router.put('/chats/:id/mark-read') // Mark conversation read
-router.delete('room/:id') // Delete by Room ID
-router.delete('message/:id') // Delete by Message ID
+router.get('/chats', chat.getMessage)
+
 
 module.exports = router
