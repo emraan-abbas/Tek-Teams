f03553171be92eda01431040a7aef577e007a0d2
emraan-abbas <imranabbas67@gmail.com>
2022-06-20 21:04:36 +0500

    new endpoints

--- Routes/chat.routes.js
+++ Routes/chat.routes.js
@@ -4,6 +4,8 @@ const router = express();
 const chat = require('../Controllers/chat.controller')
 
 router.get('/chats', chat.getMessage)
+router.post('/post-chat', chat.postMessage)
+router.post('/find', chat.getMessageById)
 
 
 module.exports = router
